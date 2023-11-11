846b1b8767072aec87b65cf34b90d78b27051168
Mahi samia <mahisamia13@gmail.com>
2023-11-11 16:07:56 +0100

    Hero section updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
         </div>
 
         <div className='flex flex-row justify-between items-center w-full'>
-          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]'>
+          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[49px] text-white ss:leading-[100.8px] leading-[75px]'>
             The Next <br className='sm:block hidden' />{" "}
             <span className='text-gradient'>Generation</span>{" "}
           </h1>
@@ -47,10 +47,8 @@ const Hero = () => {
           alt='billing'
           className='w-[100%] h-[100%] object-contain relative z-[5]'
         />
-
-        <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
-        <div className='absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40' />
-        <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
+        <div className='absolute w-full h-full top-0 left-0 z-0 blue__gradient'></div>
+        <div className='absolute w-1/2 h-1/2 bottom-0 left-0 z-1 pink__gradient'></div>
       </div>
 
       <div className={`ss:hidden ${styles.flexCenter}`}>
